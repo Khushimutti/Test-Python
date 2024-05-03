a0f8561c9ffdd84b31b0f19063f27f84f9490f86
Khushimutti <khushi.mutti@arintech.in>
2024-05-03 09:25:33 +0000

    Final

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+  name='studentXX_flask_app',
+  version='1.0.0',
+  packages=find_packages(),
+  include_package_data=True,
+  py_modules=['main'], # Explicitly include main.py
+  install_requires=[
+    'Flask>=3.0.3',
+    # Add other dependencies here
+  ],
+)
