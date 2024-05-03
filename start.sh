18718e8e47f3bdbce745a484d940751cb244526e
Khushimutti <khushi.mutti@arintech.in>
2024-05-03 06:51:37 +0000

    test

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 # /bin/bash
 python3 -m venv venv
 . ./venv/bin/activate
-#source venv/bin/activate
-#env
 python3 -m pip install -r requirements.txt
 pip3 install --upgrade pytest Flask
 pytest hello_world_test.py
-#pip3 install flask
-#pip3 show flask
-python3 main.py
+nohup python3 main.py &
