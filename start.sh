cf82a51d7af25ed4f1025edd87fea4a673d48ba2
Khushimutti <khushi.mutti@arintech.in>
2024-05-03 06:45:01 +0000

    test

--- start.sh
+++ start.sh
@@ -6,4 +6,6 @@ env
 python3 -m pip install -r requirements.txt
 pip3 install --upgrade pytest Flask
 pytest hello_world_test.py
+pip3 install flask
+pip3 show flask
 nohup python3 main.py > ~/flasklogs.log 2>&1 &
