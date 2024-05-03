58e91885f4762e07d296a5f382f3a83d7cd05084
Khushimutti <khushi.mutti@arintech.in>
2024-05-03 09:21:47 +0000

    Final

--- start.sh
+++ start.sh
@@ -5,3 +5,5 @@ python3 -m pip install -r requirements.txt
 pip3 install --upgrade pytest Flask
 pytest hello_world_test.py
 JENKINS_NODE_COOKIE=dontKillMe nohup python3 main.py > ~/flasklogs.log 2>&1 &
+pip3 install setuptools
+python3 setup.py sdist
